aadfa8ed23621f93136c798126a1ee1a7d4fba9f
muhammednazeer <nazeer4u2c@gmail.com>
2022-01-23 18:02:52 +0100

    Select element from the DOM

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//Initial values
+const API_KEY = '06b1e9fe218434d21b484d4ee3d86969';
+const endpoint = 'https://api.themoviedb.org/3/search/movie?api_key=06b1e9fe218434d21b484d4ee3d86969&query';
+
+
+//Selecting elements from the DOM
+const buttonElement = document.querySelector('#search');
+const inputElement = document.querySelector('#inputValue');
