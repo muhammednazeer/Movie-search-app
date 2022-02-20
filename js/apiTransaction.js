fe93b6652bf26e794646c7b4120ee037d4b2338b
muhammednazeer <nazeer4u2c@gmail.com>
2022-02-20 11:43:26 +0100

    function to generate search url

--- js/apiTransaction.js
+++ js/apiTransaction.js
@@ -3,3 +3,8 @@ const API_KEY = '06b1e9fe218434d21b484d4ee3d86969';
 const endpoint = 'https://api.themoviedb.org/3/search/movie?api_key=06b1e9fe218434d21b484d4ee3d86969&query';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
+function generateUrl(path) {
+    const url = `https://api.themoviedb.org/3${path}?api_key=06b1e9fe218434d21b484d4ee3d86969`;
+    return url;
+}
+
