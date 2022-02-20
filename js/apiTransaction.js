6fe2a6eb52927ccd595b25c06aa23ace73d8d160
muhammednazeer <nazeer4u2c@gmail.com>
2022-02-20 11:41:46 +0100

    constant value added to the new file

---
+++ js/apiTransaction.js
@@ -0,0 +1,5 @@
+//Initial values
+const API_KEY = '06b1e9fe218434d21b484d4ee3d86969';
+const endpoint = 'https://api.themoviedb.org/3/search/movie?api_key=06b1e9fe218434d21b484d4ee3d86969&query';
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
