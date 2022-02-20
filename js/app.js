3339ac5c02d4fee18bbf36d02372724dc46b0427
muhammednazeer <nazeer4u2c@gmail.com>
2022-02-20 11:50:56 +0100

    select some DOM element for manipulation

--- js/app.js
+++ js/app.js
@@ -1,27 +1,12 @@
-//Initial values
-const API_KEY = '06b1e9fe218434d21b484d4ee3d86969';
-const endpoint = 'https://api.themoviedb.org/3/search/movie?api_key=06b1e9fe218434d21b484d4ee3d86969&query';
-
 
 //Selecting elements from the DOM
 const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#inputValue');
+const moviesearchable = document.querySelector('#movies-searchable');
+const moviesContainer = document.querySelector('#movies-container');
+const mainContent = document.querySelector('.main');
 
 
 
 
-function search(event) {
-    event.preventDefault();
-    const value = inputElement.value;
-    const url = `${endpoint}=${value}`;
-    console.log(url);
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log('Data ', data)
-        })
-        .catch(error => {
-            console.log('Error ', error);
-        });
-  
-}
+
