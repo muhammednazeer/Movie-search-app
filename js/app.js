38d77c9bbc677edf295ea6f3fd249f7610873eea
muhammednazeer <nazeer4u2c@gmail.com>
2022-02-20 11:57:09 +0100

    movieSection and createMovieContainer functions add

--- js/app.js
+++ js/app.js
@@ -8,3 +8,36 @@ const mainContent = document.querySelector('.main');
 
 buttonElement.addEventListener('click', search);
 
+function movieSection(movies) {
+    const section = document.createElement('section');
+    section.classList = 'section';
+    movies.map((movie) => {
+        if (movie.poster_path) {
+            const img = document.createElement('img');
+            img.src = IMAGE_URL + movie.poster_path
+            img.setAttribute('data-movie-id', movie.id);
+            section.appendChild(img);
+        }
+       
+    })
+    return section;
+}
+
+function createMovieContainer(movies, title = '') {
+    const movieElement = document.createElement('div');
+    movieElement.setAttribute('class', 'movie');
+
+    const header = document.createElement('h2');
+    header.innerHTML = title;
+    const content = document.createElement('div');
+    content.classList = 'content';
+    const contentClose = `<p id="content-close">X</p>`;
+    content.innerHTML = contentClose;
+    const section = movieSection(movies);
+    
+    movieElement.appendChild(header);
+    movieElement.appendChild(section);
+    movieElement.appendChild(content);
+    return movieElement;
+}
+
